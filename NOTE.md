e95f069c8d35c441ebf7eabbbd79cca1174a09c3
rafashiga <rafa.shiga@gmail.com>
2020-03-28 18:21:46 -0300

    feat: tasks

---
+++ NOTE.md
@@ -0,0 +1,19 @@
+# Anotações
+
+- Module
+
+```
+  nest g module tasks
+```
+
+- Controller
+
+```
+  nest g controller tasks --no-spec
+```
+
+- Service
+
+```
+  nest g service tasks --no-spec
+```
---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,24 @@
+import { Task } from './task.model';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.taskService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.taskService.getTaskById(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.taskService.createTask(createTaskDto);
+  }
+}
