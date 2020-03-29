8adc260fc9dd6dc920632577df07758f8d80c5a8
rafashiga <rafa.shiga@gmail.com>
2020-03-28 21:58:27 -0300

    feat: add validation delete task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -69,6 +69,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+    const found = this.getTaskById(id);
+
+    this.tasks = this.tasks.filter(task => task.id !== found.id);
   }
 }
