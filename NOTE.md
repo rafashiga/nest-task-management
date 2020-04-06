1f80e8f4ad5b601e4e1f9abce44ff56bf612fbba
rafashiga <rafa.shiga@gmail.com>
2020-04-06 14:13:34 -0300

    feat: add logger

--- NOTE.md
+++ NOTE.md
@@ -5,3 +5,7 @@
 ```
   yarn add @nestjs/jwt @nestjs/passport passport passport-jwt
 ```
+
+## Logging
+
+- https://docs.nestjs.com/techniques/logger
--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -4,6 +4,7 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './task-status.enum';
 import { GetTasksFilterDto } from './dto/get-taks-filter.dto';
 import { User } from '../auth/user.entity';
+import { Logger, InternalServerErrorException } from '@nestjs/common';
 
 /**
  * Creating a repository allows for a place to contain our database-interaction related logic.
@@ -12,6 +13,8 @@ import { User } from '../auth/user.entity';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
+  private logger = new Logger('TaskRepository');
+
   async getTasks(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
@@ -29,9 +32,18 @@ export class TaskRepository extends Repository<Task> {
       );
     }
 
-    const tasks = await query.getMany();
-
-    return tasks;
+    try {
+      const tasks = await query.getMany();
+      return tasks;
+    } catch (error) {
+      this.logger.error(
+        `Failed to get tasks for user ${
+          user.username
+        }, Filters: ${JSON.stringify(filterDto)} `,
+        error.stack,
+      );
+      throw new InternalServerErrorException();
+    }
   }
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
@@ -42,10 +54,18 @@ export class TaskRepository extends Repository<Task> {
     task.description = description;
     task.status = TaskStatus.OPEN;
     task.user = user;
-    await task.save();
+
+    try {
+      await task.save();
+    } catch (error) {
+      this.logger.error(
+        `Failed to create a task for user ${user.username}. Data: ${createTaskDto}`,
+        error.stack,
+      );
+      throw new InternalServerErrorException();
+    }
 
     delete task.user;
-
     return task;
   }
 }
