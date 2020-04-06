74096858e76dbd04c497b2f1537a476226f33f3a
rafashiga <rafa.shiga@gmail.com>
2020-04-06 14:59:54 -0300

    chore: add env config

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,6 +5,9 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from './jwt-payload.interface';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -13,7 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'secretTask01',
+      secretOrKey: process.env.JWT_SECRET || jwtConfig.secret,
     });
   }
 
